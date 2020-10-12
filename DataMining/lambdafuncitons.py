4d7294628f77337726e54821d8d7126a325aec77
Collier <Cameron_Collier1@baylor.edu>
2020-10-12 17:13:28 -0500

    Class 10-12

--- DataMining/lambdafuncitons.py
+++ DataMining/lambdafuncitons.py
@@ -1,5 +1,7 @@
 ## esentially creates a funciton in a variable
+## lambdas process much much faster than functions
 
+## create a multiplier
 def testfunc(num):
     return lambda x : x * num
 
@@ -9,9 +11,9 @@ print(result1(9))
 print(result2(9))
 
 
+## create a doubler and a tripler
 def myfunc(n):
     return lambda a : a * n
-
 
 mydoubler = myfunc(2)
 mytripler = myfunc(3)
@@ -19,3 +21,26 @@ print(mydoubler(11))
 print(mytripler(11))
 
 
+## filter funciton
+## filters lists based on condition
+numbers_list = [2,6,8,10,11,4,12,7,13,17,0,3,21]
+filtered_list = list(filter(lambda num: (num > 7), numbers_list))
+print(filtered_list)
+
+
+## map function
+## applies the lambda to each element in the list
+numbers_list = [2,6,8,10,11,4,12,7,13,17,0,3,21]
+mapped_list = list(map(lambda num: num % 2, numbers_list))
+print(mapped_list)
+
+
+## more examples with different amounts of arguments
+x = lambda a : a + 10
+print(x(5))
+
+x = lambda a,b : a * b
+print(x(5,6))
+
+x = lambda a, b, c : a + b + c
+print(x(5,6,2))
